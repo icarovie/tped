91dceae621b47c551825d69eb5fb5573a67610c1
icarovie <icarovieirra@gmail.com>
2019-05-01 16:03:41 -0300

    main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,23 @@
 // Inclusão de bibliotecas padrão
 #include <iostream>
-#include <cstring>
+#include <string>
 #include <fstream>
 
 // Inclusão de .H
-#include "aluno.h"
-#include "curso.h"
+//#include "aluno.h"
+//#include "curso.h"
 
 using namespace std;
 
 int main(){
     // Variáveis Estaticas
     int qtdCursos,qtdAlunos;
-    
+
+    cin >> qtdCursos;
+    cin >> qtdAlunos;
+
+    cout <<"Quantidade de Alunos: "<< qtdAlunos <<endl;
+    cout <<"Quantidade de Cursos: "<< qtdCursos <<endl;     
     
 
     return 0;
